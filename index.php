e693d9bb261a93412939110eca52925476e814b4
jericoboban <jericoboban@gmail.com>
2025-11-12 18:43:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bubble Sort in PHP</title>
+</head>
+<body>
+    <h2>Bubble Sort Program</h2>
+    <form method="post" action="">
+        <label>Enter integers separated by commas:</label><br>
+        <input type="text" name="numbers" placeholder="e.g. 9,2,7,4,3" required><br><br>
+        
+        <label>Choose order:</label><br>
+        <input type="radio" name="order" value="asc" checked> Ascending
+        <input type="radio" name="order" value="desc"> Descending<br><br>
+
+        <input type="submit" name="submit" value="Sort Array">
+    </form>
+</body>
+</html>
